91987e7bb7feea7451a5ad87c06f13c950bfb76b
asimonia <alex.simonian@gmail.com>
2016-10-20 11:38:19 -0500

    Add styling and js

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,16 +1,55 @@
-var counter = 0;
-var outputEl = document.getElementById("outputEl");
-for (; counter < 5; counter++) {
-  // Give each person element a unique identifier
-  outputEl.innerHTML += `<div class="person__container" id="person--${counter}"></div>`;
+var bios = [
+{
+  title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+  }
+},
+{
+  title: "President",
+  name: "Thomas Jefferson",
+  bio: "Thomas Jefferson, a spokesman for democracy, was an American Founding Father, the principal author of the Declaration of Independence (1776), and the third President of the United States (1801–1809).",
+  image: "http://cp91279.biography.com/BIO_Mini-Bios_0_Thomas-Jefferson_151078_SF_HD_768x432-16x9.jpg",
+  lifespan: {
+    birth: 1743,
+    death: 1826
+  }
+},
+{
+  title: "Actor",
+  name: "William Shatner",
+  bio: " In his seven decades of television, Shatner became a cultural icon for his portrayal of James T. Kirk, Captain of the USS Enterprise, in the Star Trek franchise.",
+  image: "https://en.wikipedia.org/wiki/William_Shatner#/media/File:William_Shatner_by_Gage_Skidmore_2.jpg",
+  lifespan: {
+    birth: 1937,
+    death: 2020
+  }
+},
+{
+  title: "Musician",
+  name: "David Bowie",
+  bio: " He was a figure in popular music for over five decades, regarded by critics and musicians as an innovator, particularly for his work in the 1970s.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/e/e8/David-Bowie_Chicago_2002-08-08_photoby_Adam-Bielawski-cropped.jpg",
+  lifespan: {
+    birth: 1947,
+    death: 2016
+  }
+},
+{
+  title: "Game Programmer",
+  name: "David Crane",
+  bio: " He was best known as the designer of Pitfall! Pitfall! was a huge hit, and maintained the top slot on the Billboard charts for 64 weeks and was named video game of the year in 1982.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/4f/David_Crane_%2810453626776%29.jpg",
+  lifespan: {
+    birth: 1953,
+    death: 2031
+  }
 }
+];
 
-// Now containerEl will have elements in it
-var containerEl = document.getElementsByClassName("person__container");
 
-// Event listeners are created
-for (var i = 0; i < containerEl.length; i++) {
-  containerEl[i].addEventListener("click", function (event) {
-    // Logic to execute when the element is clicked
-  });
-});
+
