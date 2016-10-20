a3e6b5e14eaf9bd8064aa13139ebdf4455cd5e21
asimonia <alex.simonian@gmail.com>
2016-10-20 11:55:29 -0500

    Progress

--- wysiwyg.js
+++ wysiwyg.js
@@ -52,4 +52,15 @@ var bios = [
 ];
 
 
+// When you click on one of the person elements, a dotted border should appear around it.
+var container = document.getElementsByClassName("container");
+for (let i = 0; i < container.length; i++) {
+	container.item(i).addEventListener("click", function(e) {
+		document.getElementById("person" + i).classList.toggle("dotted-border");
+	});
+}
+
+// When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
+// When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
+// When you press the enter/return key when typing in the input field, then the content of the input field should immediately be blank.
 
